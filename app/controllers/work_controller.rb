11f0bc988bf070753eceea85c02d6e76c6ed9df2
Sasha Lavasov <imsashko@gmail.com>
2012-06-15 00:22:27 +0300

    Fixed formatting

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -1,11 +1,11 @@
 
 class WorkController < ApplicationController
   def index
-   @title = 'Works'
-   @works = Work.order("created_at asc").all
+   # @title = 'Works'
+   # @works = Work.order("created_at asc").all
   end
 
   def show
-   @work = Work.find_by_slug(params[:id])
+   # @work = Work.find_by_slug(params[:id])
   end
 end
