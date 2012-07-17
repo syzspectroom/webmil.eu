a8773e9dff65c881496f3383b51d97425e0fca40
Sasha Lavasov <imsashko@gmail.com>
2012-07-18 00:57:41 +0300

    assets make me sad

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ WebmilEu::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( screen.css mobile.css print.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
