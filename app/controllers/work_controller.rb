dd78618b13427af85de871f8c1d2fe5fa4d2e481
Alexander Lavasov <imsashko@gmail.com>
2012-11-26 21:40:09 +0200

    Works sort change

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -2,7 +2,7 @@
 class WorkController < ApplicationController
   def index
    @page_title = '- Works'
-   @works = Work.order("created_at asc").all
+   @works = Work.order("created_at desc").all
 
   end
 
